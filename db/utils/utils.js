a282bc53f08128ecc9bf78f465fe36999cd35b50
HGartland <harlenski@hotmail.co.uk>
2019-11-25 15:11:33 +0000

    formatDates passing test to reformat time on single object array

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -6,6 +6,15 @@ exports.formatDates = list => {
   // timestamp converted into a js date object
   // everything else is unmodified
   // original array is not mutated? -- maybe forget this one
+  return list.map(object => {
+    const date = new Date(object.created_at);
+    newObj = { ...object };
+    newObj.created_at = date
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ");
+    return newObj;
+  });
 };
 
 exports.makeRefObj = list => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -12,6 +12,11 @@ describe("formatDates", () => {
     expect(formatDates(input)).to.eql(expected);
     expect(formatDates(input)).to.not.equal(input);
   });
+  it("when given an array containing a single object converts the timestamp to js date object", () => {
+    const input = [{ created_at: 1511354163389 }];
+    // const expected = [{ created_at: 1511354163389 }];
+    expect(formatDates(input)).to.eql([{ created_at: "2017-11-22 12:36:03" }]);
+  });
 });
 
 describe("makeRefObj", () => {});
