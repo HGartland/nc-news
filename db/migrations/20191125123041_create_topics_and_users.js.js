a30097baf7d9e505567c2123830f25caccf08d6b
HGartland <harlenski@hotmail.co.uk>
2019-11-25 12:51:44 +0000

    migration file set up for with users and topics tables, rollback/latest migrate commands added

---
+++ db/migrations/20191125123041_create_topics_and_users.js.js
@@ -0,0 +1,28 @@
+exports.up = function(knex) {
+  console.log("creating topics and users tables...");
+  return knex.schema
+    .createTable("topics", topicsTable => {
+      topicsTable
+        .string("slug")
+        .unique()
+        .primary();
+      topicsTable.string("description").notNullable();
+    })
+    .then(() => {
+      return knex.schema.createTable("users", usersTable => {
+        usersTable
+          .string("username")
+          .unique()
+          .primary();
+        usersTable.string("avatar_url").notNullable();
+        usersTable.string("name").notNullable();
+      });
+    });
+};
+
+exports.down = function(knex) {
+  console.log("removing topics and users tables...");
+  return knex.schema.dropTable("topics").then(() => {
+    return knex.schame.dropTable("users");
+  });
+};
