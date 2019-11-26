d2bdf00761f937656f675ba72b07b7e062401613
HGartland <harlenski@hotmail.co.uk>
2019-11-26 15:01:27 +0000

    removed getUser console.log, added handle500s error handler

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const apiRouter = require("./routers/api-router");
 const app = express();
+const { handle500s } = require("./errors");
 
+// ROUTES
 app.use("/api", apiRouter);
 
+// ERROR CATCHERS
+app.use(handle500s);
+
+// INVALID URL
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "invalid url" });
 });
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,7 +1,6 @@
 const { fetchUserByUsername } = require("../models/users-models");
 
 exports.getUser = (req, res, next) => {
-  console.log(req.params);
   fetchUserByUsername(req.params).then(user => {
     res.status(200).send({ user: user[0] });
   });
